fc9cfda4b382a56d385b1ecfa499336601dca0dd
Travis Yoder <trayoda@gmail.com>
2015-04-04 22:35:53 -0600

    finished the encryptor. it can successfully be run from the command line
    wrote tests for it

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,10 +1,51 @@
+require 'date'
 require_relative "rotator"
 
 class Encrypt
-  attr_reader :key, :date
+  attr_reader :unencrypted_message,
+              :file_output,
+              :key,
+              :date,
+              :encrypted_message
 
-  def initialize(key, date)
-    @key = key
-    @date = date
+  def initialize(file_input = "message.txt", file_output = "encrypted.txt")
+    @unencrypted_message = load_file(file_input)
+    @encrypted_message   = ""
+    @file_output         = file_output
+    @key                 = generate_key
+    @date                = generate_date
+    @rotator             = Rotator.new(key, date)
   end
+
+  def encrypt_message
+    @encrypted_message = @rotator.rotate_phrase(unencrypted_message)
+  end
+
+  def write_file
+    File.write("./#{file_output}", encrypted_message)
+    puts "Created '#{file_output}' with the key #{key} and date #{date}"
+  end
+
+  private
+
+  def load_file(file_path)
+    File.read("./#{file_path}").chomp
+  end
+
+  def generate_key
+    rand(11111..99999).to_s
+  end
+
+  def generate_date
+    Date.today.strftime("%d%m%y")
+  end
+end
+
+if ARGV.size < 2
+  puts "Please supply two arguments like:"
+  puts "'$ ruby ./lib/encrypt.rb message.txt encrypted.txt'"
+else
+  e = Encrypt.new(ARGV[0], ARGV[1])
+  e.encrypt_message
+  e.write_file
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -2,4 +2,53 @@ require_relative "test_helper"
 require_relative "../lib/encrypt"
 
 class EncryptTest < Minitest::Test
+  attr_reader :encrypter
+
+  def setup
+    srand 1234
+    input_path = "test/message.txt"
+    output_path = "test/encrypted.txt"
+
+    File.delete(output_path) if File.exist?(output_path)
+
+    @encrypter = Encrypt.new(input_path, output_path)
+  end
+
+  def test_it_has_todays_date
+    date = Date.today.strftime("%d%m%y")
+
+    assert_equal date, Encrypt.new.date
+  end
+
+  def test_it_makes_a_key
+    key = /^\d{5}$/
+
+    assert key.match(Encrypt.new.key), "Key was too long or too short"
+  end
+
+  def test_it_loads_a_file
+    message_text = "wow. such doge"
+    input_path = "test/message.txt"
+
+    new_encrypter = Encrypt.new(input_path)
+
+    assert_equal message_text, new_encrypter.unencrypted_message
+  end
+
+  def test_it_encrypts_text_from_a_file
+    expected_encryption = "p3cd37ahamwt,t"
+
+    assert_equal expected_encryption, encrypter.encrypt_message
+  end
+
+  def test_it_writes_the_encrypted_text_to_a_file
+    expected_output = "p3cd37ahamwt,t"
+
+    encrypter.encrypt_message
+    encrypter.write_file
+
+    encrypted_output = File.read("./test/encrypted.txt").chomp
+    assert_equal expected_output, encrypted_output
+  end
 end
+
