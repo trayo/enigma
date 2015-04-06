1eff3015253a8b9532151c6a3410eef168c0189d
Travis Yoder <trayoda@gmail.com>
2015-04-06 14:11:12 -0600

    removed some tests from the base encryptor that are being hit by other files

--- test/base_encryptor_test.rb
+++ test/base_encryptor_test.rb
@@ -6,24 +6,12 @@ class BaseEncryptorTest < Minitest::Test
     File.write("./message.txt", "")
   end
 
-  def test_it_has_todays_date
-    date = Date.today.strftime("%d%m%y")
-
-    assert_equal date, BaseEncryptor.new.date
-  end
-
-  def test_it_makes_a_key
-    five_random_digits = /^\d{5}$/
-
-    assert_match five_random_digits, BaseEncryptor.new.key
-  end
-
   def test_it_loads_a_file
     message_text = "wow. such doge"
     input_file = "messages/test_message.txt"
     File.write(input_file, message_text)
 
-    encrypter = BaseEncryptor.new(input_file)
+    encrypter = BaseEncryptor.new(input_file, "")
 
     assert_equal message_text, encrypter.input_text
   end
