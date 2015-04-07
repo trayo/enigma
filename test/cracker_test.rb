0d2bd50fbd98f6c78b67634e25afaa309fec8744
Travis Yoder <trayoda@gmail.com>
2015-04-07 17:31:45 -0600

    adjusted the cracker test to run faster using a smaller sample set

--- test/cracker_test.rb
+++ test/cracker_test.rb
@@ -7,7 +7,7 @@ class CrackerTest < Minitest::Test
   def setup
     @input_file = "messages/test_encrypted.txt"
     @output_file = "messages/test_cracked.txt"
-    encrypted_message = "day32v0chz,mvww5.1wz"
+    encrypted_message = "5rrn.17n52nsrkp49o1bkllu0qlo"
 
     File.delete(output_file) if File.exist?(output_file)
     File.delete(input_file) if File.exist?(input_file)
@@ -15,16 +15,19 @@ class CrackerTest < Minitest::Test
   end
 
   def test_it_cracks_a_message
-    skip
-    expected_output = "space cowboy ..end.."
-    expected_key = "22222"
-    date = "060415"
+    expected_output = "see you space cowboy ..end.."
+    expected_key = "11111"
+    date = "070415"
 
     cracker = Cracker.new(input_file, output_file, date)
     cracker.crack
 
     assert_equal expected_output, cracker.cracked_message
     assert_equal expected_key, cracker.cracked_key
+  end
+
+  def teardown
+    File.delete(input_file) if File.exist?(input_file)
   end
 end
 
