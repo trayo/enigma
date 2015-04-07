bc9c37d8e8c2dff385d7b1750db01823dbe3b34b
Travis Yoder <trayoda@gmail.com>
2015-04-07 17:31:20 -0600

    cleaned up the command line base encrypt stuff it probably needs it’s own class, but this will do for now

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -19,20 +19,48 @@ class Encrypt < BaseEncryptor
   end
 end
 
+@file_input = ARGV[0]
+@file_output = ARGV[1]
+@key = ARGV[2]
+@force_option = ARGV.last
+
+def encrypt_and_write_file(e)
+  e.encrypt
+  e.write_file
+  Printer.file_created(e.file_output, e.key, e.date)
+end
+
+def ran_with_force?
+  @force_option == "force" || @force_option == "-f"
+end
+
+def supplied_a_key?
+  @key =~ /^\d{5}$/
+end
+
+def file_doesnt_exist?
+  !File.exist?(@file_output)
+end
+
 if ARGV.size < 2
   Printer.not_enough_encrypt_arguments
-elsif !File.exist?(ARGV[1]) && !ARGV[2]
-  e = Encrypt.new(ARGV[0], ARGV[1])
-  e.encrypt
-  e.write_file
-  Printer.file_created(e.file_output, e.key, e.date)
-elsif !File.exist?(ARGV[1])
 
-elsif ARGV[2] == "force" || ARGV[2] == "-f"
-  e = Encrypt.new(ARGV[0], ARGV[1])
-  e.encrypt
-  e.write_file
-  Printer.file_created(e.file_output, e.key, e.date)
+elsif file_doesnt_exist? && !@key
+  e = Encrypt.new(@file_input, @file_output)
+  encrypt_and_write_file(e)
+
+elsif file_doesnt_exist? && supplied_a_key?
+  e = Encrypt.new(@file_input, @file_output, @key)
+  encrypt_and_write_file(e)
+
+elsif ran_with_force? && supplied_a_key?
+  e = Encrypt.new(@file_input, @file_output, @key)
+  encrypt_and_write_file(e)
+
+elsif ran_with_force?
+  e = Encrypt.new(@file_input, @file_output)
+  encrypt_and_write_file(e)
+
 else
   Printer.file_already_exists
 end
