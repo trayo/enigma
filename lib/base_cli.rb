d8151411e822affbe5486906084e9c75c4f113e4
Travis Yoder <trayoda@gmail.com>
2015-04-08 16:03:17 -0600

    made a CLI to handle all the command line stuff

---
+++ lib/base_cli.rb
@@ -0,0 +1,25 @@
+require_relative "printer"
+
+class BaseCLI
+
+  def initialize(arguments)
+    @arguments = arguments
+    @file_input = arguments[0]
+    @file_output = arguments[1]
+    @key = arguments[2]
+    @force_option = arguments.last
+  end
+
+  def ran_with_force?
+    @force_option == "force" || @force_option == "-f"
+  end
+
+  def supplied_a_key?
+    @key =~ /^\d{5}$/
+  end
+
+  def file_doesnt_exist?
+    !File.exist?(@file_output)
+  end
+end
+
