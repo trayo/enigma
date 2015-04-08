b0b51e0d5cd84069e98b4e2df8090222d7448915
Travis Yoder <trayoda@gmail.com>
2015-04-08 17:17:12 -0600

    wrote a cli cracker to handle the messages in the command line when running the cracker

---
+++ lib/cli_cracker.rb
@@ -0,0 +1,34 @@
+require_relative "cli_base"
+
+class CLICraker < CLIBase
+  def initialize(arguments)
+    super(arguments)
+    @date = arguments[2]
+  end
+
+  def run
+    if @arguments.size < 3
+      Printer.not_enough_encrypt_arguments
+    elsif file_doesnt_exist?
+      c = Cracker.new(@file_input, @file_output, @date)
+      crack_and_write_file(c)
+    elsif ran_with_force?
+      c = Cracker.new(@file_input, @file_output, @date)
+      crack_and_write_file(c)
+    else
+      Printer.file_already_exists
+    end
+  end
+
+  private
+
+  def crack_and_write_file(c)
+    c.crack
+    c.write_file
+    Printer.file_created(c.file_output, c.cracked_key, c.date)
+  end
+
+  def supplied_a_date?
+    @date =~ /^\d{6}$/
+  end
+end
